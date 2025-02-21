974a0287bf57094eadfbbb932e4b06414d8a11f1
Jack Henschel <jackdev@mailbox.org>
2025-02-21 08:56:35 +0100

    Remove flux from master branch
    See 'flux-old' branch
    
    https://git.cubieserver.de/Cubieserver/homelab/src/branch/flux-old

--- flux/scripts/reconcile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-flux -n flux-system reconcile source git homelab-apps-prod
-sleep 1
-flux -n flux-system reconcile kustomization apps-prod
--- flux/scripts/status.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "+++ SOURCES +++"
-kubectl get gitrepository,helmrepository -A
-
-echo
-echo "+++ DEPLOYMENTS +++"
-kubectl get kustomization,helmrelease -A
