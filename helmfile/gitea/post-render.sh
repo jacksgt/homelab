fc6218e3d7ba4ecbe6675c0d0e997d84579338bb
Jack Henschel <jhensche@redhat.com>
2025-10-10 08:37:20 +0200

    Deploy Anubis sidecar for Gitea
    https://github.com/TecharoHQ/anubis/blob/main/data/botPolicies.yaml
    https://anubis.techaro.lol/docs/admin/policies
    
    https://helmfile.readthedocs.io/en/latest/advanced-features/#adhoc-kustomization-of-helm-charts

---
+++ helmfile/gitea/post-render.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# This Helm post-renderer is a really ugly hack
+# to point the service to Anubis instead of Gitea directly
+# becuase the Gitea Helm chart currently does not support changing the targetPort.
+# Refs:
+# https://gitea.com/gitea/helm-gitea/issues/900
+# https://austindewey.com/2020/07/27/patch-any-helm-chart-template-using-a-kustomize-post-renderer/
+
+set -e
+cd gitea/
+cat <&0 > all.yaml
+kustomize build .
+rm all.yaml
